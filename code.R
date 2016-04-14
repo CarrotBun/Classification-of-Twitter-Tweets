70e043396cdeb0f060ae59278838b59da2e631fe
Lily <ylilyli24@gmail.com>
2016-04-14 16:38:11 -0700

    updates on log

--- code.R
+++ code.R
@@ -21,7 +21,7 @@ sum(yhat_rf == data$y[test])/length(data$y[test])
 # kaggle data scoring
 yhat_rf_xtest = predict(rf_model, Xtest)
 
-### 3. Gradient Boosting 
+### Gradient Boosting 
 library(gbm)
 
 gbm_model = gbm.fit(x = data[train,1:1000], 
@@ -32,4 +32,19 @@ gbm_model = gbm.fit(x = data[train,1:1000],
 
 yhat_gbm = predict(gbm_model, data[test,], 
                    n.trees = 10, type = "response")[,,1]
+# logistic 
+# log sparse
+library(glmnet)
+LogSparse <- cv.glmnet(as.matrix(prostateData[index, 1:6033]),
+                               as.matrix(prostateData$y[index]), 
+                               family = "binomial")
+
+# predict responsese based on regression
+log.sparse_prediction <- predict(prostateLogSparse, as.matrix(prostateData[-index, 1:6033]), lambda = "lambda.1se", type = "response")
+
+# take only predictions with more than .5 probability
+log.sparse_prediction2 <-(log.sparse_prediction > .5)
+
+# accuracy through proportion
+sum(log.sparse_prediction2 == prostateData$y[-index])/length(prostateData$y[-index])
 
