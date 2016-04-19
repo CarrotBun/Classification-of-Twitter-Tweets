a9c347b3d8e2f0d73fde0f411735025a65ce8af0
josephfrancia <josephfrancia@berkeley.edu>
2016-04-19 15:59:18 -0700

    Update FeatureSelection.r

--- FeatureSelection.r
+++ FeatureSelection.r
@@ -5,20 +5,24 @@ library(Boruta)
 library(git2r)
 library(FNN)
 library(xgboost)
+library(MASS)
+library(glmnet)
 load("/Users/itsaj77/Downloads/TrainTest.RData")
 
 #Cleaning Data 
 X=as.matrix(X)
+Xtest=as.matrix(Xtest)
+
 
 #Randomizing indices
 set.seed(1)
 indices=sample(1:50000, 50000)
-train=indices[1:10000]
-test=indices[10001:50000]
+train=indices[1:40000]
+test=indices[40001:50000]
 
 
 #Removing Highly Correlated Features 
-corr_mat=cor(X[,1:1000])
+corr_mat=cor(Xtest[,1:1000])
 high_corr=findCorrelation(corr_mat, cutoff=.5)
 print(high_corr)
 
@@ -26,14 +30,31 @@ print(high_corr)
 lasso=read.csv("/Users/itsaj77/Desktop/Classes/Stats 154/simulation_results.csv")
 columns=lasso$x
 ordered_coef=order(lasso$x, decreasing=TRUE)
-unimportant=ordered_coef[201:1000]
+unimportant_RF=ordered_coef[640:1000]
+unimportant_log=ordered_coef[245:1000]
 
 
-#Finding Intersection of Unimportant and Highly Correlated Features... Basically, if row 94 is both "highly correlated" and "unimportant", we don't want to remove it twice
-repeats=intersect(high_corr, unimportant)
-high_corr_new=high_corr[high_corr %in% unimportant==FALSE]
-unimportant_new=unimportant[unimportant %in% high_corr==FALSE]
+
+
+#Finding Intersection of Unimportant and Highly Correlated Features for RF
+repeats=intersect(high_corr, unimportant_RF)
+high_corr_new=high_corr[high_corr %in% unimportant_RF==FALSE]
+unimportant_newRF=unimportant_RF[unimportant_RF %in% high_corr==FALSE]
 
 useless=union(repeats, high_corr)
-useless=union(useless, unimportant)
-X=X[,-c(useless)]
+useless=union(useless, unimportant_newRF)
+X_RF=X[,-c(useless)]
+Xtest_RF=Xtest[, -c(useless)]
+
+
+
+#Finding Intersection of Unimportant and Highly Correlated Features for Logistic
+repeats=intersect(high_corr, unimportant_log)
+high_corr_new=high_corr[high_corr %in% unimportant_log==FALSE]
+unimportant_newlog=unimportant_log[unimportant_log %in% high_corr==FALSE]
+
+useless=union(repeats, high_corr_new)
+useless=union(useless, unimportant_newlog)
+X_log=X[,-c(useless)]
+Xtest_log=Xtest[, -c(useless)]
+
