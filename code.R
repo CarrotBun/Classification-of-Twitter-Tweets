01690257dca7a9dbe3d09fcba11b3187d986e64e
Lily <ylilyli24@gmail.com>
2016-04-25 14:16:11 -0700

    titled things

--- code.R
+++ code.R
@@ -1,6 +1,6 @@
 # data
 load("TrainTest.RData")
-
+##########################
 # random forest
 library(randomForest)
 
@@ -13,34 +13,7 @@ yhat_rf = predict(rf_model, data[test, ])
 
 sum(yhat_rf == data$y[test])/length(data$y[test])
 
-# kaggle data scoring
-yhat_rf_xtest = predict(rf_model, Xtest)
-
-### Gradient Boosting ## unfinished
-library(gbm)
-
-gbm_model = gbm.fit(x = data[train,1:1000], 
-                    y = as.factor(data$y[train]), 
-                    n.trees = 20, 
-                    distribution = "multinomial", 
-                    verbose = TRUE, shrinkage = 0.6)
-
-yhat_gbm = predict(gbm_model, data[test,], 
-                   n.trees = 10, type = "response")[,,1]4
-# xg boost ## unfinished
-library(xgboost)
-t1 = proc.time()
-rf_model2 = xgboost(data = X[train,], label = y[train], nrounds = 1, verbose = 2, 
-                    max.depth = 10, num_parallel_tree = 20, 
-                    subsample = 1., colsample_bytree = .04, 
-                    objective = "multi:softmax", num_class = 10)
-t2 = proc.time()
-yhat = predict(rf_model2, X[valid,])
-accuracy_score(y[valid], yhat)
-
-
-
-
+############################################
 # logistic 
 library(glmnet)
 log_model <- cv.glmnet(X[train, c(sim_indices[1:100, 2])], 
@@ -73,6 +46,7 @@ log_predictions_glm2 <-(log_prediction2 > .5)
 sum(log_predictions_glm2 == y[test])/length(y[test])
 
 #################################
+#function to run log regression #
 run_log = function(index){
   # log model
   predictors = c(sim_indices[1:index, 2])
@@ -91,18 +65,15 @@ run_log = function(index){
   return(accuracy)
 }
 
+# using functions
+
 run_log(50) #0.596
 run_log(100) #0.604
 run_log(500) #0.572
 run_log(800) #0.572
 
-vec = seq(100,1000,100)
-output = vector()
-for(i in 1:length(vec)){
-  num = vec[i]
-  output[i] = run_log(num)
-}
 
+# plot of logistic predictors
 plot(vec2, output2, type = "l", 
      main = "# of Predictors vs. Acccuracy Rate for Logistic Regression", 
      xlab = "# of predictors",
@@ -116,7 +87,7 @@ for(i in 1:length(vec2)){
   output2[i] = run_log(num)
 }
 
-
+##################################################
 # Naive Bayes
 library(e1071)
 nBayes_model = naiveBayes(x = X[train, c(sim_indices[1:100, 2])], y = as.factor(y[train]))
@@ -130,3 +101,25 @@ nBayes_prediction2 <- predict(nBayes_model2,
                              newdata = X[test, ]) 
 # accuracy
 sum(nBayes_prediction2 == y[test])/length(y[test])
+
+
+#######################################
+library(caret)
+library(ggplot2)
+control = trainControl(method = "repeatedcv", number = 10, repeats = 3)
+# train the SVM model
+SVM_model = train(as.factor(y[train])~X[train, c(sim_indices[1:100, 2])], 
+                  data = cbind(X[train, c(sim_indices[1:100, 2])], y[train]),
+                  method = "svmRadial",
+                  trControl = control)
+GBM_model = train(as.factor(y[train])~X[train, c(sim_indices[1:100, 2])], 
+                  data = cbind(X[train, c(sim_indices[1:100, 2])], y[train]),
+                  method = "gbm",
+                  trControl = control,
+                  verbose = FALSE)
+
+NB_model = train(as.factor(y[train])~X[train, c(sim_indices[1:100, 2])], 
+                  data = cbind(X[train, c(sim_indices[1:100, 2])], y[train]),
+                  method = "nbDiscrete",
+                  trControl = control)
+bwplot(resamples(list(GBM = GBM_model, SVM = SVM_model)))
